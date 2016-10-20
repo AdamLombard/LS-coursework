597812550b3deb54f75c695c1247dd3800e0f350
Adam Lombard <willcodeforgin@gmail.com>
2016-10-19 23:05:40 -0700

    starting easy 1 exercises

---
+++ 101/lesson_03/easy1/Q02.rb
@@ -0,0 +1,21 @@
+# Difference between ! and ? in Ruby
+# "!" can mean "not" or "mutate caller", depending on the context
+# "?" indicates that a method returns a boolean
+
+# what is != and where should you use it?
+# Means "is not equal to", and should be used when comparing values
+
+# put ! before something, like !user_name
+# Means "not value"
+
+# put ! after something, like words.uniq!
+# Means "mutate the caller"
+
+# put ? before something
+# I don't know what this means...
+
+# put ? after something
+# Means this method returns a Boolean
+
+# put !! before something, like !!user_name
+# Huh. No clue...
