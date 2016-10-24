6c09b9f529dc65c613bcd315134c98536390c884
Adam Lombard <willcodeforgin@gmail.com>
2016-10-24 08:15:04 -0700

    finished Hard1

--- 101/lesson_03/hard1/Q04.rb
+++ 101/lesson_03/hard1/Q04.rb
@@ -10,4 +10,7 @@ def uuid
   uuid
 end
 
-# Got locked on to the idea of making a one-line
+# Got locked on to the idea of making a one-line method for this...
+def uuid
+  Array.new(32) { rand(16).to_s(16) }.join.unpack('a8a4a4a4a*').join('-')
+end
