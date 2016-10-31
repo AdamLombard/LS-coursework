a69ac7be6fe0b3bbc7f4ba78ef168c82e6b90fab
Adam Lombard <willcodeforgin@gmail.com>
2016-10-30 19:17:15 -0700

    continue adding bonus features to tic-tac-toe

--- 101/lesson_04/tictactoe_bonus.rb
+++ 101/lesson_04/tictactoe_bonus.rb
@@ -1,40 +1,31 @@
-require 'pry'
-
-class String
-  def colorize(code)
-    "\e[#{code}m#{self}\e[0m"
-  end
-
-  def pink
-    colorize(35)
-  end
-
-  def blue
-    colorize(36)
-  end
-end
-
 GAME_WIDTH      = 48
 SEPARATOR       = ("-" * GAME_WIDTH)
-MARKER_PLAYER   = 'X'.pink.freeze
-MARKER_COMPUTER = 'O'.blue.freeze
+MARKER_PLAYER   = 'X'.freeze
+MARKER_COMPUTER = 'O'.freeze
 MARKER_EMPTY    = ' '.freeze
 GRID_LINE_FULL  = "-----+-----+-----".freeze
 GRID_LINE_EMPTY = "     |     |".freeze
 WINNING_LINES   = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                   [[1, 5, 9], [3, 5, 7]]              # diagonals
+PLAYER_USER     = "Player".freeze
+PLAYER_COMP     = "Computer".freeze
+PLAYER_NONE     = "None".freeze
+PLAYER_FIRST    = PLAYER_COMP
+WINNER_USER     = "#{PLAYER_USER} wins!".freeze
+WINNER_COMP     = "#{PLAYER_COMP} wins!".freeze
+WINNER_NONE     = "It's a tie!".freeze
 
 def prompt(msg)
   puts "=> " + msg
 end
 
-# rubocop:disable Metrics/AbcSize
-def display_board(brd)
+def display_board(brd, user_score, comp_score)
   system 'clear'
 
   puts SEPARATOR
-  puts "Player (#{MARKER_PLAYER}): Computer (#{MARKER_COMPUTER}):"
+  puts "Player (#{MARKER_PLAYER}): #{user_score}" \
+       "Computer (#{MARKER_COMPUTER}): #{comp_score}"
   puts SEPARATOR
   puts GRID_LINE_EMPTY
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -49,16 +40,15 @@ def display_board(brd)
   puts GRID_LINE_EMPTY
   puts
 end
-# rubocop:enable Metrics/AbcSize
 
 def initialze_board
   new_board = {}
-  (1..9).each { |n| new_board[n] = n }
+  (1..9).each { |n| new_board[n] = MARKER_EMPTY }
   new_board
 end
 
 def empty_squares(board)
-  board.keys.select { |k| board[k].to_i > 0 }
+  board.keys.select { |k| board[k] == MARKER_EMPTY}
 end
 
 def joinor(arr, delim=',', word_before_last='or')
@@ -78,7 +68,29 @@ def player_places_piece!(board)
 end
 
 def computer_places_piece!(board)
-  square        = empty_squares(board).sample
+  square = nil
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, board, MARKER_COMPUTER)
+    break if square
+  end
+
+  if !square
+    WINNING_LINES.each do |line|
+      square = find_at_risk_square(line, board, MARKER_PLAYER)
+      break if square
+    end
+  end
+
+  if !square
+    if board[5] == MARKER_EMPTY
+      square = 5
+    end
+  end
+
+  if !square
+    square = empty_squares(board).sample
+  end
+
   board[square] = MARKER_COMPUTER
 end
 
@@ -93,33 +105,82 @@ end
 def detect_winner(board)
   WINNING_LINES.each do |line|
     if board.values_at(*line).count(MARKER_PLAYER) == 3
-      return 'Player'
+      return WINNER_USER
     elsif board.values_at(*line).count(MARKER_COMPUTER) == 3
-      return 'Computer'
+      return WINNER_COMP
     end
   end
   nil
 end
 
+def find_at_risk_square(line, board, marker)
+  if board.values_at(*line).count(marker) == 2
+    board.select{ |k,v| line.include?(k) && v == MARKER_EMPTY }.keys.first
+  else
+    nil
+  end
+end
+
+def choose_player
+  prompt "Who goes first? (P)layer or (C)omputer?"
+  loop do
+    first_player = gets.chomp
+    break if first_player == "P" || first_player == "C"
+    prompt "Please enter (P) or (C)..."
+  end
+  convert_first_player(first_player)
+end
+
+def convert_first_player(first_player)
+  first_player == 'P' ? PLAYER_USER : PLAYER_COMP
+end
+
+
+user_score = 0
+comp_score = 0
+first_player = PLAYER_FIRST
+first_player = choose_player if first_player == PLAYER_NONE
 loop do
   board = initialze_board
 
   loop do
-    display_board(board)
+    display_board(board, user_score, comp_score)
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    case first_player
+    when PLAYER_USER
+      player_places_piece!(board)
+      display_board(board, user_score, comp_score)
+      break if someone_won?(board) || board_full?(board)
 
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+      computer_places_piece!(board)
+      display_board(board, user_score, comp_score)
+      break if someone_won?(board) || board_full?(board)
+    when PLAYER_COMP
+      computer_places_piece!(board)
+      display_board(board, user_score, comp_score)
+      break if someone_won?(board) || board_full?(board)
+
+      player_places_piece!(board)
+      display_board(board, user_score, comp_score)
+      break if someone_won?(board) || board_full?(board)
+    end
   end
 
-  display_board(board)
+  display_board(board, user_score, comp_score)
 
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+    case detect_winner(board)
+    when WINNER_USER
+      user_score += 1
+      display_board(board, user_score, comp_score)
+      prompt WINNER_USER
+    when WINNER_COMP
+      comp_score += 1
+      display_board(board, user_score, comp_score)
+      prompt WINNER_COMP
+    end
   else
-    prompt "It's a tie!"
+    prompt WINNER_NONE
   end
 
   prompt "Play again? (Y/N)"
